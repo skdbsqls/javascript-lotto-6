69f1430f8cdc1f6ee00450a479a0e6e4f183a911
skdbsqls <skdbsqls98@naver.com>
2023-12-13 16:42:26 +0900

    feat: 구입 가격 및 로또 번호 예외 처리 로직 작성

---
+++ src/utils/validation/LottoValidate.js
@@ -0,0 +1,80 @@
+import { ERROR_MESSAGE } from "../../constants/message.js";
+
+// 로또 예외 처리
+const LottoValidate = {
+  // 당첨 번호 예외 처리
+  WinNumValidate(winNum) {
+    // 쉼표로 구분하지 않은 경우
+    LottoCheck.checkSeparate(winNum);
+
+    // 구분한 경우
+    const winNumArr = MakeArray(winNum);
+
+    LottoCheck.checkNumber(winNumArr);
+    LottoCheck.checkRange(winNumArr);
+    LottoCheck.checkLengthWin(winNumArr);
+    LottoCheck.duplicateWin(winNumArr);
+  },
+
+  // 보너스 번호 예외 처리
+  BonusNumValidate(winNum, bonusNum) {
+    const winNumArr = MakeArray(winNum);
+    const bonusNumArr = [...bonusNum];
+
+    LottoCheck.checkNumber(bonusArr);
+    LottoCheck.checkRange(bonusArr);
+
+    if (winNumArr.includes(bonusNumArr)) {
+      throw new Error(ERROR_MESSAGE.duplicateBonus);
+    }
+  },
+};
+
+// 배열로 만들기
+const MakeArray = (nums) => {
+  return nums
+    .split(",")
+    .map((num) => num.trim())
+    .sort((a, b) => a - b);
+};
+
+const LottoCheck = {
+  // 쉼표로 구분하지 않은 경우
+  checkSeparate(nums) {
+    if (nums.includes(",")) {
+      throw new Error(ERROR_MESSAGE.notSeparate);
+    }
+  },
+  // 숫자가 아닌 경우
+  checkNumber(nums) {
+    nums.forEach((num) => {
+      if (isNaN(num)) {
+        throw new Error(ERROR_MESSAGE.notNumLotto);
+      }
+    });
+  },
+  // 1~45까지 숫자가 아닌 경우
+  checkRange(nums) {
+    nums.forEach((num) => {
+      if (num < 1 || num > 45) {
+        throw new Error(ERROR_MESSAGE.invalidRange);
+      }
+    });
+  },
+  // 6개가 아닌 경우
+  checkLengthWin(nums) {
+    if (nums.length !== 6) {
+      throw new Error(ERROR_MESSAGE.invalidLength);
+    }
+  },
+  // 중복되는 경우
+  checkDuplicate(nums) {
+    const set = [...new Set(nums)];
+
+    if (nums.length !== set.length) {
+      throw new Error(ERROR_MESSAGE.duplicateWin);
+    }
+  },
+};
+
+export default LottoValidate;
---
+++ src/utils/validation/PriceValidate.js
@@ -0,0 +1,24 @@
+import { ERROR_MESSAGE } from "../../constants/message.js";
+
+// 구입 금액 예외 처리
+const PriceValidate = (price) => {
+  PriceCheck.notNum(price);
+  PriceCheck.checkUnit(price);
+};
+
+const PriceCheck = {
+  // 숫자가 아닌 경우
+  checkNumber(price) {
+    if (isNaN(price)) {
+      throw new Error(ERROR_MESSAGE.notNum);
+    }
+  },
+  // 1000원 단위로 입력하지 않은 경우
+  checkUnit(price) {
+    if (price % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGE.invalidUnit);
+    }
+  },
+};
+
+export default PriceValidate;
